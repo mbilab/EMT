803969f7d91fabd6b778b11865cc078c633df5f3
goho302jo03 <goho302jo03@gmail.com>
2017-07-14 14:44:22 +0800

    app component

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+div
+  h1 hello
+  h1 hello
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="sass" scoped>
+h1
+  font-size: 3rem
+</style>
